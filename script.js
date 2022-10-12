5cd748a93ff829ed46b0913b173383da81f3d54f
gacz1205 <gacz1205@gmail.com>
2022-10-12 09:53:06 -0500

    Inicializando el respositorio

---
+++ script.js
@@ -0,0 +1,16 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const result = document.querySelector('#result');
+
+// btn.addEventListener('click', btnOnClick);
+
+// function btnOnClick(){
+//     const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+//     result.innerHTML = "Resultado " + sumaInputs;
+// }
+
+// function inputOnChange(){
+//     console.log('cambio el input')
+// }   
